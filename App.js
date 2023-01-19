b903268f4237ae3af282e877673ee61fb6d9095a
Jay <jaypithava@xhtmljunkies.com>
2023-01-19 15:27:12 +0530

    final commmit

--- App.js
+++ App.js
@@ -1,33 +1,121 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView, Button ,Alert , TouchableHighlight } from 'react-native';
-import { useState , useEffect  } from 'react';
-import * as LocalAuthentication from 'expo-local-authentication';
-
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Button,
+  Alert,
+  TouchableHighlight,
+} from "react-native";
+import { useState, useEffect } from "react";
+import * as LocalAuthentication from "expo-local-authentication";
 
 export default function App() {
-const[isBiometricSupported, SetIsBiometricSupported] = useState(false)
+  const [isBiometricSupported, SetIsBiometricSupported] = useState(false);
 
-//Face detection and Fingerprint Scan
-useEffect(() => {
-  (async () => {
-    const compatible = await LocalAuthentication.hasHardwareAsync();
-    SetIsBiometricSupported(compatible)
-  })
-},[])
+  //Face detection and Fingerprint Scan
+  useEffect(() => {
+    (async () => {
+      const compatible = await LocalAuthentication.hasHardwareAsync();
+      SetIsBiometricSupported(compatible);
+    })();
+  });
+
+  const fallBackToDefaultAuth = () => {
+    console.log("Fallback to password authentication");
+  };
+
+  const alertComponent = (title, mess, btnText, btnFunc) => {
+    return Alert.alert(title, mess, [
+      {
+        text: btnText,
+        onPress: btnFunc,
+      },
+    ]);
+  };
+
+  const TwoButtonAlert = () => {
+    Alert.alert("Welcome To App", "Okk", [
+      {
+        text: "Back",
+        onPress: () => console.log("Cancel Pressed"),
+        style: "cancel",
+      },
+      {
+        text: "Ok",
+        onPress: () => console.log("OK Pressed"),
+      },
+    ]);
+  };
+
+  const handleBiometricAuth = async () => {
+    const isBiometricAvailable = await LocalAuthentication.hasHardwareAsync();
+    if (!isBiometricAvailable)
+      return alertComponent(
+        "Please Enter Your Password",
+        "Biometric Authentication Not Supported",
+        "Ok",
+        () => fallBackToDefaultAuth()
+      );
+
+    //Check Biometric Type Available
+    let supportedBiometrics;
+    if (isBiometricAvailable)
+      supportedBiometrics =
+        await LocalAuthentication.supportedAuthenticationTypesAsync();
+
+    //Check biometric type available in user's Device
+    const savedBiometrics = await LocalAuthentication.isEnrolledAsync();
+    if (!savedBiometrics)
+      return alertComponent(
+        "Biometric Record Not Found",
+        "Please Enter Your Password",
+        "Ok",
+        () => fallBackToDefaultAuth()
+      );
+
+    //Authenticate With Biometric
+    const biometricAuth = await LocalAuthentication.authenticateAsync({
+      promptMessage: "Login with Biometric Authentication",
+      cancelLabel: "Cancel",
+      disableDeviceFallback: true,
+    });
+
+    //Log The User On Success
+    if (biometricAuth) {
+      TwoButtonAlert();
+    }
+    console.log(isBiometricAvailable);
+    console.log(supportedBiometrics);
+    console.log(savedBiometrics);
+    console.log(biometricAuth);
+  };
 
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView>
+      <View style={styles.container}>
+        <Text>
+          {isBiometricSupported
+            ? "Your Device Compatible with Biometric Authentication"
+            : "Face or Fingerprint Available on this device"}
+        </Text>
+        <TouchableHighlight style={{ height: 60, marginTop: 200 }}>
+          <Button
+            title='Login With Biometrics'
+            color='black'
+            onPress={handleBiometricAuth}
+          />
+        </TouchableHighlight>
+        <StatusBar style='auto' />
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop : StatusBar.currentStatusBar
   },
 });
