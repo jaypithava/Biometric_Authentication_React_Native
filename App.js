074a209f1091c4152d3a1131ab5b51fc9de51a0b
Jay <jaypithava@xhtmljunkies.com>
2023-01-19 14:57:42 +0530

    first commit

--- App.js
+++ App.js
@@ -1,7 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Button ,Alert , TouchableHighlight } from 'react-native';
+import { useState , useEffect  } from 'react';
+import * as LocalAuthentication from 'expo-local-authentication';
+
 
 export default function App() {
+const[isBiometricSupported, SetIsBiometricSupported] = useState(false)
+
+//Face detection and Fingerprint Scan
+useEffect(() => {
+  (async () => {
+    const compatible = await LocalAuthentication.hasHardwareAsync();
+    SetIsBiometricSupported(compatible)
+  })
+},[])
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
